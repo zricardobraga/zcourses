d14b5f2e86c1720de9eb707ed770c4c5b2f8f8a7
zricardobraga <zericardobraga@gmail.com>
2024-02-13 10:41:50 -0300

    :tada: - project create

---
+++ src/main/java/com/zcourses/zcourses/ZcoursesApplication.java
@@ -0,0 +1,13 @@
+package com.zcourses.zcourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZcoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZcoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zcourses/zcourses/ZcoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zcourses.zcourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZcoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
