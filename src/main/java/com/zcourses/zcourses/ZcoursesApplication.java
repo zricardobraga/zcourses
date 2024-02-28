5597d72ccf0d4a6177e768efef31e7bc32e64aee
zricardobraga <zericardobraga@gmail.com>
2024-02-28 18:33:08 -0300

    :sparkles: - implemented documentantion (swagger)

--- src/main/java/com/zcourses/zcourses/ZcoursesApplication.java
+++ src/main/java/com/zcourses/zcourses/ZcoursesApplication.java
@@ -1,9 +1,16 @@
 package com.zcourses.zcourses;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Z Courses",
+		description = "Course API",
+		version = "1"
+))
 public class ZcoursesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/zcourses/zcourses/modules/course/controller/CourseController.java
+++ src/main/java/com/zcourses/zcourses/modules/course/controller/CourseController.java
@@ -3,7 +3,15 @@ package com.zcourses.zcourses.modules.course.controller;
 import com.zcourses.zcourses.modules.course.CourseEntity;
 import com.zcourses.zcourses.modules.course.StatusEnum;
 import com.zcourses.zcourses.modules.course.useCases.CourseUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.hibernate.annotations.Array;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +23,21 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/courses")
+@Tag(name = "Curso")
 public class CourseController {
 
     @Autowired
     private CourseUseCase courseUseCase;
 
     @PostMapping("/create")
+    @Operation(summary = " Cadastro", description = "Essa rota é responsável por cadastrar o curso no sistema")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = CourseEntity.class)
+                    )
+            }),
+            @ApiResponse(responseCode = "400", description = "Course not found")
+    })
     public ResponseEntity<Object> create (@Valid @RequestBody CourseEntity courseEntity) {
         try {
             courseEntity.setStatus(StatusEnum.ACTIVE);
@@ -35,6 +52,14 @@ public class CourseController {
     }
 
     @GetMapping("/get")
+    @Operation(summary = "Listagem", description = "Essa rota é responsável por listar todas as vagas disponíveis")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(array = @ArraySchema (schema = @Schema(implementation = CourseEntity.class)
+                    ))
+            }),
+            @ApiResponse(responseCode = "204", description = "No content")
+    })
     public ResponseEntity<List<CourseEntity>> get(){
         List<CourseEntity> courses = new ArrayList<>();
         courses = this.courseUseCase.getAll();
@@ -46,6 +71,14 @@ public class CourseController {
     }
 
     @PutMapping("/update/{id}")
+    @Operation(summary = "Atualiza as informações do curso", description = "Essa rota é responsável por atualizar as informações do curso, exceto o Status")
+    @ApiResponses({
+            @ApiResponse(responseCode = "202", content = {
+                    @Content(schema = @Schema(implementation = CourseEntity.class)
+                    )
+            }),
+            @ApiResponse(responseCode = "400", description = "Course not found")
+    })
     public ResponseEntity<Object> update(@PathVariable UUID id, @Valid @RequestBody Map<String, Object> data){
 
         if (data.get("name").toString().isEmpty() || data.get("category").toString().isEmpty()) {
@@ -58,13 +91,27 @@ public class CourseController {
     }
 
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Deleta o curso", description = "Essa rota é responsável por deletar o curso")
+    @ApiResponses({
+            @ApiResponse(responseCode = "202", description = "Deleted course"),
+            @ApiResponse(responseCode = "400", description = "Course not found")
+    })
     public ResponseEntity<Object> delete(@PathVariable UUID id){
         var result = this.courseUseCase.delete(id);
 
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.ok().body("Deleted course");
     }
 
     @PatchMapping("/update-status/{id}")
+    @PutMapping("/update/{id}")
+    @Operation(summary = "Atualiza o status do curso", description = "Essa rota é responsável por atualizar o Status do curso")
+    @ApiResponses({
+            @ApiResponse(responseCode = "202", content = {
+                    @Content(schema = @Schema(implementation = CourseEntity.class)
+                    )
+            }),
+            @ApiResponse(responseCode = "400", description = "Course not updated")
+    })
     public ResponseEntity<Object> updateStatus(@PathVariable UUID id, @RequestBody Map<String, Object> data){
         try {
             CourseEntity updatedCourse = this.courseUseCase.updateStatus(id, data);
